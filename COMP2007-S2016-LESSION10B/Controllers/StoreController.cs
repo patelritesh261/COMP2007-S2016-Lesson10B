8a138e5e188eec62734d893139939820f978a141
Ritesh Patel <patelritesh261@outlook.com>
2016-07-21 15:10:10 -0400

    Updated details

--- COMP2007-S2016-LESSION10B/Controllers/StoreController.cs
+++ COMP2007-S2016-LESSION10B/Controllers/StoreController.cs
@@ -22,9 +22,10 @@ namespace COMP2007_S2016_LESSION10B.Controllers
         }
         //
         // GET: /Store/Browse?genre=Disco
-        public ActionResult Browse(string genre)
+        public ActionResult Browse(string genre ="Rock")
         {
-            Genre genreModel = new Genre(genre);
+            // Retrieve Genre and its Associated Albums from database
+            Genre genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
 
             return View(genreModel);
         }
@@ -32,7 +33,7 @@ namespace COMP2007_S2016_LESSION10B.Controllers
         // GET: /Store/Details/5
         public ActionResult Details(int id = 1)
         {
-            Album album = new Album("Album " + id);
+            Album album = storeDB.Albums.Find(id);
 
             return View(album);
         }
