be7516b8e055d865e4bf9db6a337bf036ceca89d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-07-21 13:24:38 -0400

    updated project files

--- COMP2007-S2016-Lesson10B/Controllers/HomeController.cs
+++ COMP2007-S2016-Lesson10B/Controllers/HomeController.cs
@@ -8,23 +8,13 @@ namespace COMP2007_S2016_Lesson10B.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /Home/
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
