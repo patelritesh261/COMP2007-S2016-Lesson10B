6688fa479c4dd52da479f84813f9a49c4e929142
Ritesh Patel <patelritesh261@outlook.com>
2016-07-21 13:47:14 -0400

    Added the Artist Model Classes

---
+++ COMP2007-S2016-LESSION10B/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP2007_S2016_LESSION10B.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
